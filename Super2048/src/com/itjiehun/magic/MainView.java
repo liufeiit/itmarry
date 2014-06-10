bb9f718b6cc62a2df380a7209c70535daa879fd6
liufeiit <liufei_it@126.com>
2014-06-10 18:55:05 +0800

    update

--- Super2048/src/com/itjiehun/magic/MainView.java
+++ Super2048/src/com/itjiehun/magic/MainView.java
@@ -11,9 +11,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
 import android.view.View;
 
 public class MainView extends View {
@@ -144,19 +147,75 @@ public class MainView extends View {
 	}
 	
 	private final Bitmap 
-			mouse = BitmapFactory.decodeResource(getResources(), R.drawable.mouse),
-			cow = BitmapFactory.decodeResource(getResources(), R.drawable.cow),
-			tiger = BitmapFactory.decodeResource(getResources(), R.drawable.tiger),
-			rabbit = BitmapFactory.decodeResource(getResources(), R.drawable.rabbit),
-			dragon = BitmapFactory.decodeResource(getResources(), R.drawable.dragon),
-			snake = BitmapFactory.decodeResource(getResources(), R.drawable.snake),
-			horse = BitmapFactory.decodeResource(getResources(), R.drawable.horse),
-			sheep = BitmapFactory.decodeResource(getResources(), R.drawable.sheep),
-			monkey = BitmapFactory.decodeResource(getResources(), R.drawable.monkey),
-			chicken = BitmapFactory.decodeResource(getResources(), R.drawable.chicken),
-			dog = BitmapFactory.decodeResource(getResources(), R.drawable.dog),
-			pig = BitmapFactory.decodeResource(getResources(), R.drawable.pig)
+			mouse = decodeResource(getResources(), R.drawable.mouse),
+			cow = decodeResource(getResources(), R.drawable.cow),
+			tiger = decodeResource(getResources(), R.drawable.tiger),
+			rabbit = decodeResource(getResources(), R.drawable.rabbit),
+			dragon = decodeResource(getResources(), R.drawable.dragon),
+			snake = decodeResource(getResources(), R.drawable.snake),
+			horse = decodeResource(getResources(), R.drawable.horse),
+			sheep = decodeResource(getResources(), R.drawable.sheep),
+			monkey = decodeResource(getResources(), R.drawable.monkey),
+			chicken = decodeResource(getResources(), R.drawable.chicken),
+			dog = decodeResource(getResources(), R.drawable.dog),
+			pig = decodeResource(getResources(), R.drawable.pig)
 			;
+	
+//	private Bitmap d(Resources resources, int id) {
+//		Bitmap bmp = BitmapFactory.decodeResource(resources, id);
+//		Bitmap newb = Bitmap.createBitmap(cellSize, cellSize, Config.ARGB_8888);
+//		Canvas canvasTemp = new Canvas(newb);
+//		canvasTemp.drawBitmap(bmp, 50, 50, p);
+//	}
+	
+	private Bitmap decodeResource(Resources resources, int id) {
+	    TypedValue value = new TypedValue();
+	    resources.openRawResource(id, value);
+	    BitmapFactory.Options opts = setBitmapOption(resources, id, cellSize, cellSize);
+	    opts.inTargetDensity = value.density;
+	    return BitmapFactory.decodeResource(resources, id);
+	}
+	
+	private static BitmapFactory.Options setBitmapOption(Resources resources, int id, int width, int height) {
+        BitmapFactory.Options opt = new BitmapFactory.Options();
+        opt.inJustDecodeBounds = true;
+        // 设置只是解码图片的边距，此操作目的是度量图片的实际宽度和高度
+//        BitmapFactory.decodeFile(file, opt);
+        BitmapFactory.decodeResource(resources, id, opt);
+        int outWidth = opt.outWidth; // 获得图片的实际高和宽
+        int outHeight = opt.outHeight;
+        opt.inDither = false;
+        opt.inPreferredConfig = Bitmap.Config.RGB_565;
+        // 设置加载图片的颜色数为16bit，默认是RGB_8888，表示24bit颜色和透明通道，但一般用不上
+        opt.inSampleSize = 1;
+        // 设置缩放比,1表示原比例，2表示原来的四分之一....
+        // 计算缩放比
+        if (outWidth != 0 && outHeight != 0 && width != 0 && height != 0) {
+            int sampleSize = (outWidth / width + outHeight / height) / 2;
+            opt.inSampleSize = sampleSize;
+        }
+        opt.inJustDecodeBounds = false;// 最后把标志复原
+        return opt;
+    }
+	
+//  GameView.drawImage(canvas, mBitDestTop, miDTX, mBitQQ.getHeight(), mBitDestTop.getWidth(), mBitDestTop.getHeight()/2, 0, 0);
+  public void drawImage(Canvas canvas, Bitmap blt, int x, int y, int w, int h, int bx, int by) {//x,y表示绘画的起点，
+      Rect src = new Rect();// 图片
+      Rect dst = new Rect();// 屏幕位置及尺寸
+      //src 这个是表示绘画图片的大小
+      src.left = bx;   //0,0  
+      src.top = by;
+      src.right = bx + w;// mBitDestTop.getWidth();,这个是桌面图的宽度，
+      src.bottom = by + h;//mBitDestTop.getHeight()/2;// 这个是桌面图的高度的一半
+      // 下面的 dst 是表示 绘画这个图片的位置
+      dst.left = x;    //miDTX,//这个是可以改变的，也就是绘图的起点X位置
+      dst.top = y;    //mBitQQ.getHeight();//这个是QQ图片的高度。 也就相当于 桌面图片绘画起点的Y坐标
+      dst.right = x + w;    //miDTX + mBitDestTop.getWidth();// 表示需绘画的图片的右上角
+      dst.bottom = y + h;    // mBitQQ.getHeight() + mBitDestTop.getHeight();//表示需绘画的图片的右下角
+      canvas.drawBitmap(blt, src, dst, paint);//这个方法  第一个参数是图片原来的大小，第二个参数是 绘画该图片需显示多少。也就是说你想绘画该图片的某一些地方，而不是全部图片，第三个参数表示该图片绘画的位置
+      src = null;
+      dst = null;
+  }
 	
 	private void createBitmapCells() {
 		paint.setTextSize(cellTextSize);
@@ -172,7 +231,11 @@ public class MainView extends View {
 			} else {
 				paint.setColor(TEXT_BLACK);
 			}
-			canvas.drawBitmap(getBitmap(value), 10, 10, paint);
+			
+//			drawImage(canvas, getBitmap(value), 0, 0, cellSize, cellSize, 0, 0);
+			RectF rectF = new RectF(0, 0, cellSize, cellSize);
+			canvas.drawBitmap(getBitmap(value), null, rectF, null);
+//			canvas.drawBitmap(getBitmap(value), 10, 10, paint);
 			
 //			drawCellText(canvas, value, 0, 0);
 			
