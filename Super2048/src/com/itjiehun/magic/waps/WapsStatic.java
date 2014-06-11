7d2e04c1faa0f279d1f41c79ffd723e13a9e2125
liufeiit <liufei_it@126.com>
2014-06-11 18:51:10 +0800

    update

---
+++ Super2048/src/com/itjiehun/magic/waps/WapsStatic.java
@@ -0,0 +1,14 @@
+package com.itjiehun.magic.waps;
+
+/**
+ * 
+ * @author 刘飞 E-mail:liufei_it@126.com
+ * @version 1.0
+ * @since 2014-6-11 下午5:53:28
+ */
+public interface WapsStatic {
+
+	String APP_ID = "504b8a14540c17008187807847143e9a";
+	
+	String APP_PID = "default";
+}
