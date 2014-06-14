38343b4fe1e3976348487b8cb26187acf2df2f39
liufeiit <liufei_it@126.com>
2014-06-14 23:20:52 +0800

    fan

---
+++ fan/src/com/itjiehun/magic/umeng/UmengStatic.java
@@ -0,0 +1,20 @@
+package com.itjiehun.magic.umeng;
+
+/**
+ * 
+ * @author 刘飞 E-mail:liufei_it@126.com
+ * @version 1.0
+ * @since 2014-6-10 下午1:41:05
+ */
+public interface UmengStatic {
+	
+	String ADS_KEY = "hiapk_ads_key";
+	
+	String XM_ADS_KEY = "xiaomi_ads_key";
+	
+	String V360_ADS_KEY = "v360_ads_key";
+
+	String UMENG_APPKEY = "539be48f56240b3ccd06111f";
+	
+	String UMENG_CHANNEL = "baidu";
+}
